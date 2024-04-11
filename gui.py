34402d4043c6d41408fc5461d28871b622cee725
David.Alobo <davidalobo01@gmail.com>
2024-04-11 05:41:31 +0100

    Complete and Exit Button added

--- gui.py
+++ gui.py
@@ -3,8 +3,56 @@ import PySimpleGUI as sg
 
 label = sg.Text('To_Do App')
 add_button = sg.Button('Add')
-input_box = sg.InputText(tooltip='Enter todo')
+input_box = sg.InputText(tooltip='Enter todo', key='todo')
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
-window = sg.Window('My To-Do App', layout=[[label],[input_box, add_button]])
+window = sg.Window('My To-Do App', 
+                   layout=[[label], 
+                           [input_box, add_button], 
+                           [list_box, edit_button, complete_button]],
+                   font=('Helvetica', 20))
 window.read()
 window.close()
+
+while True:
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todos'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['tofos'.update](values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = function.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'[0]])
+        
+        case sg.WIN_CLOSED:
+            break
+
+window.close()
+
+
